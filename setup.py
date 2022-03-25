c9cda052bb5baccedb514461e7c62386b998034d
Alex <alexhliu02@gmail.com>
2022-03-25 12:56:50 -0700

    Re-add requirements.txt for docker.

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from pkg_resources import parse_requirements
 
 setup(
     name='Chunky Discord Bot',
@@ -8,9 +9,5 @@ setup(
     description='Friendly bot for the Chunky Discord server.',
     url='https://github.com/chunky-dev/discord-bot',
     packages=['src'],
-    install_requires=[
-        "discord.py~=1.7.3",
-        "discord-py-slash-command~=3.0.3",
-        "PyGithub~=1.55",
-    ],
+    install_requires=parse_requirements('requirements.txt'),
 )
